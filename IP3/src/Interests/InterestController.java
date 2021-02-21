0a07296166965526051215bf87d027c0a9751ee7
erinoconnor <erinoconnor2000@outlook.com>
2021-02-21 20:33:11 +0000

    interests

--- IP3/src/Interests/InterestController.java
+++ IP3/src/Interests/InterestController.java
@@ -5,10 +5,45 @@
  */
 package Interests;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
 /**
  *
  * @author erino
  */
-public class InterestController {
+public class InterestController implements Initializable{
+    @FXML
+    private JFXDatePicker birthday;
+
+    @FXML
+    private JFXTextField firstName;
+
+    @FXML
+    private JFXTextField userEmail;
+
+    @FXML
+    private JFXTextField secondName;
+
+    @FXML
+    private JFXComboBox<?> uniChoice;
     
+    
+    
+    
+    
+    
+    
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+
+    }
 }
