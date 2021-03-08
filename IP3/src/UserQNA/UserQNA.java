0025252c1d4f0c3942844a87ac835a681fb616e8
Zubair <zubairkhalid12@gmail.com>
2021-03-08 14:22:53 +0000

    added files

---
+++ IP3/src/UserQNA/UserQNA.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserQNA;
+
+import ip3.User;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Zuby
+ */
+public class UserQNA extends Application{
+
+    User currentUser;
+
+    public void start(Stage stage) throws Exception {
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("userQNAScreen.fxml"));
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        UserQNAController controller = loader.getController();
+        controller.setData(currentUser);
+        stage.setScene(scene);
+        stage.setTitle("UserQNA");
+        stage.show();
+        stage.centerOnScreen();
+    }
+
+ public UserQNA(User user) {
+        currentUser=user;
+    }
+
+   
+}
